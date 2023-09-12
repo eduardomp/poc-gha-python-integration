3f41ebf3fbf303cef6e31726f531b9aee39d3e5f
Eduardo Medeiros Pereira <edu.medeirospereira@gmail.com>
2023-09-12 00:17:52 +0000

    changing the environment variable format and the python code to get the correct list format

--- print_emails.py
+++ print_emails.py
@@ -2,8 +2,11 @@ import os
 import json
 
 def print_emails():
-    email_list=os.environ.get('EMAIL_LIST','').strip('][').split(',')
+    email_list=os.environ.get('EMAIL_LIST','').split(',')
     email_admin_fixed=os.environ.get('EMAIL_ADMIN','')
+    
+    print(email_list)
+    print(f'The type of email_list is : {type(email_list)}')
     print(f'Emails in the list: {email_list}')
     print(f'First email in the list: {email_list[0]}')
     print(f'Fixed email in the workflow: {email_admin_fixed}')
